4e3db98825bb811ea0283cc31e9a14f03e885556
chu pack <91909231+DaneX3089@users.noreply.github.com>
2022-12-11 23:24:25 +0400

    Add files via upload

---
+++ Refresher.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <Windows.h>
+using namespace std;
+int main() {
+    cout << "Click CTRL+B to enable. CTRL+X to disable";
+    while (1) {
+        Sleep(1);
+        bool enabler = false;
+        if (GetAsyncKeyState(VK_LCONTROL) && GetAsyncKeyState(0x42)) {
+            enabler = true;
+        }
+        while (enabler == true) {
+            keybd_event(VK_LCONTROL, 0, 0, 0);
+            keybd_event(VK_F5, 0, 0, 0);
+            Sleep(50);
+            keybd_event(VK_LCONTROL, 0, KEYEVENTF_KEYUP, 0);
+            keybd_event(VK_F5, 0, KEYEVENTF_KEYUP, 0);
+            Sleep(100);
+            if (GetAsyncKeyState(VK_LCONTROL) && GetAsyncKeyState(0x58)) {
+                enabler = false;
+              
+            }
+            
+        
+        }
+        
+    }
+ 
+}
